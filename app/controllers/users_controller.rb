1ac2ef3c2655513bcd673c7c0e7131f45105ac6e
koske0125 <79013492+koske0125@users.noreply.github.com>
2022-04-10 04:30:42 +0000

    [Update]UserEdit機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,23 @@ class UsersController < ApplicationController
     @books = @user.books
   end
 
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
